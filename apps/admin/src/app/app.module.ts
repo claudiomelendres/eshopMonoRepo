65e9ceaaff7f026141d7a0157ef90ca10a9de7e5
claudiomelendres <claudiomelendres@gmail.com>
2022-04-12 23:15:14 -0400

    categories

---
+++ apps/admin/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+import {CardModule} from 'primeng/card';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import { HttpClientModule } from '@angular/common/http';
+import { CategoriesService } from '@bluebits/products';
+
+const UX_MODULE = [
+    CardModule,
+    ToolbarModule,
+    ButtonModule,
+    TableModule
+]
+
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'categories',
+                component: CategoriesListComponent
+            }
+        ]
+
+    }
+]
+
+@NgModule({
+    declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+    imports: [BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+        ...UX_MODULE ],
+    providers: [CategoriesService],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
