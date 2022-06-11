85e3cbd472e742385b5cff6580b0b8cb2b160b74
claudiomelendres <claudiomelendres@gmail.com>
2022-06-10 22:59:49 -0400

    orders

--- apps/admin/src/app/pages/orders/orders-details/orders-details.component.ts
+++ apps/admin/src/app/pages/orders/orders-details/orders-details.component.ts
@@ -1,16 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Order, OrdersService } from '@bluebits/orders';
+import { MessageService } from 'primeng/api';
+import { ORDER_STATUS } from '../order.constants';
 
 @Component({
-  selector: 'admin-order-detail',
-  templateUrl: './orders-details.component.html',
-  styles: [
-  ]
+    selector: 'admin-order-detail',
+    templateUrl: './orders-details.component.html',
+    styles: []
 })
 export class OrdersDetailsComponent implements OnInit {
+    order: Order;
+    orderStatuses = [];
+    selectedStatus: any;
 
-  constructor() { }
+    constructor(private orderService: OrdersService, private messageService: MessageService, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this._mapOrderStatus();
+        this._getOrder();
+    }
 
+    private _mapOrderStatus() {
+        this.orderStatuses = Object.keys(ORDER_STATUS).map((key) => {
+            return {
+                id: key,
+                name: ORDER_STATUS[key].label
+            };
+        });
+    }
+
+    private _getOrder() {
+        this.route.params.subscribe((params) => {
+            if (params.id) {
+                this.orderService.getOrder(params.id).subscribe((order) => {
+                    this.order = order;
+                    this.selectedStatus = order.status;
+                });
+            }
+        });
+    }
+
+    onStatusChange(event) {
+        this.orderService.updateOrder({ status: event.value }, this.order.id).subscribe({
+            next: () => {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'Order is updated!'
+                });
+            },
+            error: () => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: 'Order is not updated!'
+                });
+            }
+        });
+    }
 }
