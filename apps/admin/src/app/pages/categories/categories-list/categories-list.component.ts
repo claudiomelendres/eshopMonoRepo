54c4eb67c6dd26bbea2c0515442bb4b570087d5c
claudiomelendres <claudiomelendres@gmail.com>
2022-06-15 21:24:04 -0400

    login (#5)

--- apps/admin/src/app/pages/categories/categories-list/categories-list.component.ts
+++ apps/admin/src/app/pages/categories/categories-list/categories-list.component.ts
@@ -1,57 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Category } from '@bluebits/products';
 import { CategoriesService } from '@bluebits/products';
-import {ConfirmationService, MessageService} from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'admin-categories-list',
-  templateUrl: './categories-list.component.html',
-  styles: [
-  ]
+    selector: 'admin-categories-list',
+    templateUrl: './categories-list.component.html',
+    styles: []
 })
-export class CategoriesListComponent implements OnInit {
-  categories: Category[] = [];
+export class CategoriesListComponent implements OnInit, OnDestroy {
+    categories: Category[] = [];
 
-  constructor(private categoriesService: CategoriesService,
-              private messageService: MessageService,
-              private confirmationService: ConfirmationService,
-              private router: Router
+    endsubs$: Subject<any> = new Subject();
 
-    ) { }
+    constructor(
+        private categoriesService: CategoriesService,
+        private messageService: MessageService,
+        private confirmationService: ConfirmationService,
+        private router: Router
+    ) {}
 
-  ngOnInit(): void {
-    this._getCategories();
-  }
+    ngOnDestroy(): void {
+        // this.endsubs$.next();
+        this.endsubs$.complete();
+    }
 
-  deleteCategory(categoryId: string) {
-    this.confirmationService.confirm({
-      message: 'Do you want to Delete this Category?',
-      header: 'Delete Category',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.categoriesService.deleteCategory(categoryId).subscribe(
-          {
-            next: () => {
-              this._getCategories();
-              this.messageService.add({severity: 'success', summary: 'Success', detail:'Category is deleted'});
-            },
-            error: () => this.messageService.add({ severity:'error', summary: 'Error', detail:'Category could not be deleted'})
-          },
+    ngOnInit(): void {
+        this._getCategories();
+    }
 
-        );
-      }})}
+    deleteCategory(categoryId: string) {
+        this.confirmationService.confirm({
+            message: 'Do you want to Delete this Category?',
+            header: 'Delete Category',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.categoriesService.deleteCategory(categoryId).subscribe({
+                    next: () => {
+                        this._getCategories();
+                        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Category is deleted' });
+                    },
+                    error: () => this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Category could not be deleted' })
+                });
+            }
+        });
+    }
 
-  private _getCategories() {
-    this.categoriesService.getCategories().subscribe( cats => {
-      console.log(cats);
-      
-      this.categories = cats;
-    })
-  }
+    private _getCategories() {
+        this.categoriesService
+            .getCategories()
+            .pipe(takeUntil(this.endsubs$))
+            .subscribe((cats) => {
+                console.log(cats);
 
-  updateCategory(categoryId: string) {
-    this.router.navigateByUrl(`categories/form/${categoryId}`)
-  }
+                this.categories = cats;
+            });
+    }
 
+    updateCategory(categoryId: string) {
+        this.router.navigateByUrl(`categories/form/${categoryId}`);
+    }
 }
--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
-export const usersRoutes: Route[] = [];
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
-    declarations: [
-      LoginComponent
-    ]
+    imports: [CommonModule, RouterModule.forChild(routes), InputTextModule, ButtonModule, FormsModule, ReactiveFormsModule],
+    declarations: [LoginComponent]
 })
 export class UsersModule {}
